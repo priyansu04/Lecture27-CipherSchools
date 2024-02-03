e357ecbeba252ae4a1ec00db81ae6373b42e18bb
priyansu04 <91240406+priyansu04@users.noreply.github.com>
2024-02-04 03:43:11 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {3, 7, 10, 11, 4, 1};
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            //find index of minimum element
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+
+            }
+
+//the index of min element is found in minIndex
+//now we will swap the sin element and bring it in its place,
+// i.e. swap elements at minIndex and i
+
+            int temp = arr[i];
+
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+//Here our array should be sorted
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+
+            System.out.println();
+        }
+    }
+}
